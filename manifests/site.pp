d424806af7f1aa641ab5cf7a97f13868640799c9
ashusaxena04 <58472573+ashusaxena04@users.noreply.github.com>
2019-12-03 20:15:04 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,7 @@
 node default {
   file {'/root/README':
     ensure => file,
-    content => 'This is a Readme file'
-    owner => 'root'
+    content => 'This is a Readme file',
+    owner => 'root',
     }
 }
